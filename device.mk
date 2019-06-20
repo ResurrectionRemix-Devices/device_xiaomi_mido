95d0dc4d895d537f637a56bf2651e47e3cb33a14
ZeeLog <Danjel.Zammit@gmail.com>
2019-06-20 11:58:24 +0200

    mido: FM Radio: Refactor & Cleanup
    Change-Id: I802bc8f892c1db165d8d86b410f56780d63dae44
    
     Conflicts:
    	proprietary-files-qc.txt
    	rootdir/etc/init.qcom.rc

--- device.mk
+++ device.mk
@@ -304,7 +304,6 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.goodix.sh \
     init.qcom.bt.sh \
-    init.qti.fm.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
